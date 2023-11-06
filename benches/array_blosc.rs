2fecce9ac926f7266aad28d3637ea0a548d55590
Lachlan Deakin <ljdgit@gmail.com>
2023-11-06 21:42:49 +1100

    Add array_blosc benchmarks

---
+++ benches/array_blosc.rs
@@ -0,0 +1,86 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use zarrs::array::codec::{
+    bytes_to_bytes::blosc::{BloscCompressionLevel, BloscShuffleMode},
+    BloscCodec,
+};
+
+fn array_blosc_write_all(c: &mut Criterion) {
+    let mut group = c.benchmark_group("array_blosc_write_all");
+    for size in [128u64, 256u64, 512u64].iter() {
+        let num_elements: u64 = size * size * size;
+        group.throughput(Throughput::Bytes(num_elements));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| {
+                let store = zarrs::storage::store::MemoryStore::new();
+                let array = zarrs::array::ArrayBuilder::new(
+                    vec![size; 3],
+                    zarrs::array::DataType::UInt8,
+                    vec![32; 3].into(),
+                    zarrs::array::FillValue::from(0u8),
+                )
+                .bytes_to_bytes_codecs(vec![Box::new(
+                    BloscCodec::new(
+                        zarrs::array::codec::bytes_to_bytes::blosc::BloscCompressor::BloscLZ,
+                        BloscCompressionLevel::try_from(9).unwrap(),
+                        None,
+                        BloscShuffleMode::BitShuffle,
+                        Some(2),
+                    )
+                    .unwrap(),
+                )])
+                .build(store.into(), "/")
+                .unwrap();
+                let data = vec![1u8; num_elements.try_into().unwrap()];
+                let subset = zarrs::array_subset::ArraySubset::new_with_shape(vec![size; 3]);
+                array
+                    .par_store_array_subset_elements(&subset, data)
+                    .unwrap();
+            });
+        });
+    }
+    group.finish();
+}
+
+fn array_blosc_read_all(c: &mut Criterion) {
+    let mut group = c.benchmark_group("array_blosc_read_all");
+    for size in [128u64, 256u64, 512u64].iter() {
+        let num_elements: u64 = size * size * size;
+        group.throughput(Throughput::Bytes(num_elements));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            // Write the data
+            let store = zarrs::storage::store::MemoryStore::new();
+            let array = zarrs::array::ArrayBuilder::new(
+                vec![size; 3],
+                zarrs::array::DataType::UInt8,
+                vec![32; 3].into(),
+                zarrs::array::FillValue::from(0u8),
+            )
+            .bytes_to_bytes_codecs(vec![Box::new(
+                BloscCodec::new(
+                    zarrs::array::codec::bytes_to_bytes::blosc::BloscCompressor::BloscLZ,
+                    BloscCompressionLevel::try_from(9).unwrap(),
+                    None,
+                    BloscShuffleMode::BitShuffle,
+                    Some(2),
+                )
+                .unwrap(),
+            )])
+            .build(store.into(), "/")
+            .unwrap();
+            let data = vec![1u8; num_elements.try_into().unwrap()];
+            let subset = zarrs::array_subset::ArraySubset::new_with_shape(vec![size; 3]);
+            array
+                .par_store_array_subset_elements(&subset, data)
+                .unwrap();
+
+            // Benchmark reading the data
+            b.iter(|| {
+                let _bytes = array.par_retrieve_array_subset(&subset).unwrap();
+            });
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, array_blosc_write_all, array_blosc_read_all);
+criterion_main!(benches);
--- benches/array_uncompressed.rs
+++ benches/array_uncompressed.rs
@@ -39,14 +39,14 @@ fn array_write_all_sharded(c: &mut Criterion) {
                 let sharding_codec = Box::new(ShardingCodecBuilder::new(vec![32; 3]).build());
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
-                    zarrs::array::DataType::UInt8,
+                    zarrs::array::DataType::UInt16,
                     vec![size; 3].into(),
-                    zarrs::array::FillValue::from(0u8),
+                    zarrs::array::FillValue::from(0u16),
                 )
                 .array_to_bytes_codec(sharding_codec)
                 .build(store.into(), "/")
                 .unwrap();
-                let data = vec![1u8; num_elements.try_into().unwrap()];
+                let data = vec![1u16; num_elements.try_into().unwrap()];
                 let subset = zarrs::array_subset::ArraySubset::new_with_shape(vec![size; 3]);
                 array
                     .par_store_array_subset_elements(&subset, data)
@@ -67,13 +67,13 @@ fn array_read_all(c: &mut Criterion) {
             let store = zarrs::storage::store::MemoryStore::new();
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
-                zarrs::array::DataType::UInt8,
+                zarrs::array::DataType::UInt16,
                 vec![32; 3].into(),
-                zarrs::array::FillValue::from(0u8),
+                zarrs::array::FillValue::from(0u16),
             )
             .build(store.into(), "/")
             .unwrap();
-            let data = vec![1u8; num_elements.try_into().unwrap()];
+            let data = vec![1u16; num_elements.try_into().unwrap()];
             let subset = zarrs::array_subset::ArraySubset::new_with_shape(vec![size; 3]);
             array
                 .par_store_array_subset_elements(&subset, data)
