36eef68456e9a3fff103fedb505d9928b79682f5
Lachlan Deakin <ljdgit@gmail.com>
2024-07-03 21:27:27 +1000

    Add missing `zarr_format` to `GroupMetadataV2`

--- src/metadata/v2/group.rs
+++ src/metadata/v2/group.rs
@@ -7,12 +7,14 @@ use crate::metadata::AdditionalFields;
 #[derive(Serialize, Deserialize, Clone, Eq, PartialEq, Debug, Display, From)]
 #[display(fmt = "{}", "serde_json::to_string(self).unwrap_or_default()")]
 pub struct GroupMetadataV2 {
+    /// An integer defining the version of the storage specification to which the group adheres. Must be `2`.
+    pub zarr_format: usize,
     /// Optional user metadata.
     #[serde(default, flatten)]
     pub attributes: serde_json::Map<String, serde_json::Value>,
     /// Additional fields.
     ///
-    /// These are not part of Zarr V2, but are retrained for compatibility/flexibility.
+    /// These are not part of Zarr V2, but are retained for compatibility/flexibility.
     #[serde(default, flatten)]
     pub additional_fields: AdditionalFields,
 }
