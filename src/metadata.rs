2910b5e0c5edc7cef29c5e9f52cfeb44c358cb79
Lachlan Deakin <ljdgit@gmail.com>
2024-07-28 11:11:51 +1000

    Remove outdated note in `metadata` docs

--- src/metadata.rs
+++ src/metadata.rs
@@ -3,7 +3,6 @@
 //! [`ArrayMetadata`] and [`GroupMetadata`] can hold any conformant array/group metadata.
 //!
 //! All known array metadata is defined in this module, even if `zarrs` has not been compiled with the appropriate flags to use it.
-//! An exception is the configuration of experimental codecs, which are feature gated.
 
 mod array;
 mod group;
