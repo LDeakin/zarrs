b82141d09c7a99b8cc0693cd182344103fd58a35
Lachlan Deakin <ljdgit@gmail.com>
2024-09-18 19:44:41 +1000

    Fix license links in crate root docs

--- zarrs/src/lib.rs
+++ zarrs/src/lib.rs
@@ -170,8 +170,8 @@
 //!
 //! ## Licence
 //! `zarrs` is licensed under either of
-//!  - the Apache License, Version 2.0 [LICENSE-APACHE](./LICENCE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0> or
-//!  - the MIT license [LICENSE-MIT](./LICENCE-MIT) or <http://opensource.org/licenses/MIT>, at your option.
+//!  - the Apache License, Version 2.0 [LICENSE-APACHE](https://docs.rs/crate/zarrs/latest/source/LICENCE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0> or
+//!  - the MIT license [LICENSE-MIT](https://docs.rs/crate/zarrs/latest/source/LICENCE-MIT) or <http://opensource.org/licenses/MIT>, at your option.
 //!
 //! Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
 
