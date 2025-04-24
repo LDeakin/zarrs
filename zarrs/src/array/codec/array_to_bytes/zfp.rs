bccfa842b5a97f4b68aeca38f8b6f2275af349ed
Lachlan Deakin <ljdgit@gmail.com>
2025-04-25 07:37:11 +1000

    move zfp warning to top

--- zarrs/src/array/codec/array_to_bytes/zfp.rs
+++ zarrs/src/array/codec/array_to_bytes/zfp.rs
@@ -1,11 +1,11 @@
 //! The `zfp` array to bytes codec.
 //!
-//! [zfp](https://zfp.io/) is a compressed number format for 1D to 4D arrays of 32/64-bit floating point or integer data.
-//! 8/16-bit integer types are supported through promotion to 32-bit in accordance with the [zfp utility functions](https://zfp.readthedocs.io/en/release1.0.1/low-level-api.html#utility-functions).
-//!
 //! <div class="warning">
 //! This is a registered codec that originated in `zarrs`. It may not be supported by other Zarr V3 implementations.
 //! </div>
+//!
+//! [zfp](https://zfp.io/) is a compressed number format for 1D to 4D arrays of 32/64-bit floating point or integer data.
+//! 8/16-bit integer types are supported through promotion to 32-bit in accordance with the [zfp utility functions](https://zfp.readthedocs.io/en/release1.0.1/low-level-api.html#utility-functions).
 //!
 //! This codec requires the `zfp` feature, which is disabled by default.
 //!
