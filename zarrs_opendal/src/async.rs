17089e670d34eb1592075e735d007cbfbd7ead4c
Lachlan Deakin <ljdgit@gmail.com>
2025-04-25 15:36:13 +1000

    chore: add async tests for storage adapters

--- zarrs_opendal/src/async.rs
+++ zarrs_opendal/src/async.rs
@@ -201,8 +201,22 @@ impl AsyncListableStorageTraits for AsyncOpendalStore {
 
 #[cfg(test)]
 mod tests {
+    use zarrs_storage::storage_adapter::{
+        async_to_sync::{AsyncToSyncBlockOn, AsyncToSyncStorageAdapter},
+        performance_metrics::PerformanceMetricsStorageAdapter,
+        usage_log::UsageLogStorageAdapter,
+    };
+
     use super::*;
-    use std::error::Error;
+    use std::{error::Error, sync::Arc};
+
+    struct TokioBlockOn(tokio::runtime::Runtime);
+
+    impl AsyncToSyncBlockOn for TokioBlockOn {
+        fn block_on<F: core::future::Future>(&self, future: F) -> F::Output {
+            self.0.block_on(future)
+        }
+    }
 
     #[tokio::test]
     async fn memory() -> Result<(), Box<dyn Error>> {
@@ -212,6 +226,39 @@ mod tests {
         zarrs_storage::store_test::async_store_write(&store).await?;
         zarrs_storage::store_test::async_store_read(&store).await?;
         zarrs_storage::store_test::async_store_list(&store).await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn memory_adapters() -> Result<(), Box<dyn Error>> {
+        let builder = opendal::services::Memory::default();
+        let op = Operator::new(builder)?.finish();
+        let store = Arc::new(AsyncOpendalStore::new(op));
+        let store = Arc::new(PerformanceMetricsStorageAdapter::new(store));
+        let log_writer = Arc::new(std::sync::Mutex::new(
+            // std::io::BufWriter::new(
+            std::io::stdout(),
+            //    )
+        ));
+        let store = Arc::new(UsageLogStorageAdapter::new(store, log_writer, || {
+            "".to_string()
+        }));
+        zarrs_storage::store_test::async_store_write(&store).await?;
+        zarrs_storage::store_test::async_store_read(&store).await?;
+        zarrs_storage::store_test::async_store_list(&store).await?;
+        Ok(())
+    }
+
+    #[test]
+    fn memory_sync() -> Result<(), Box<dyn Error>> {
+        let builder = opendal::services::Memory::default();
+        let op = Operator::new(builder)?.finish();
+        let store = Arc::new(AsyncOpendalStore::new(op));
+        let store =
+            AsyncToSyncStorageAdapter::new(store, TokioBlockOn(tokio::runtime::Runtime::new()?));
+        zarrs_storage::store_test::store_write(&store)?;
+        zarrs_storage::store_test::store_read(&store)?;
+        zarrs_storage::store_test::store_list(&store)?;
         Ok(())
     }
 
